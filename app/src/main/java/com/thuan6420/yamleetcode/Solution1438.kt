780ab66301a9b39fd80269857aa2756e3a51307a
thuan6420 <thuan6420@gmail.com>
2024-06-24 20:25:10 +0700

    #1438 - update deque to run exactly

--- app/src/main/java/com/thuan6420/yamleetcode/Solution1438.kt
+++ app/src/main/java/com/thuan6420/yamleetcode/Solution1438.kt
@@ -1,46 +1,38 @@
 package com.thuan6420.yamleetcode
 
+import java.util.Deque
+import java.util.LinkedList
 import kotlin.math.abs
 
 // 1438. Longest Continuous Subarray With Absolute Diff Less Than or Equal to Limit
 class Solution1438 {
     fun longestSubarray(nums: IntArray, limit: Int): Int {
+        val maxDeque: Deque<Int> = LinkedList()
+        val minDeque: Deque<Int> = LinkedList()
         var result = 0
-        val isAbsoluteDiffLessThanLimit: (Int, Int) -> Boolean = { a, b -> abs(a - b) <= limit }
-        val maxOfResult: (Int, Int) -> Int = { a, b -> if (a > b) a else b }
-        val calculateResult: (Int, Int, Int) -> Int = { max, min, size ->
-            if (isAbsoluteDiffLessThanLimit(max, min)) {
-                size
-            } else {
-                0
+        var numChecking = 0
+        while (numChecking < nums.size) {
+            while (!maxDeque.isEmpty() && nums[numChecking] > nums[maxDeque.peekLast()]) {
+                maxDeque.pollLast()
             }
-        }
-        for (startIndex in nums.indices) {
-            for (endIndex in startIndex until nums.size) {
-                var max = nums[startIndex]
-                var min = nums[startIndex]
-                for (i in startIndex ..  endIndex) {
-                    max = maxOf(max, nums[i])
-                    min = minOf(min, nums[i])
+            while (!minDeque.isEmpty() && nums[numChecking] < nums[minDeque.peekLast()]) {
+                minDeque.pollLast()
+            }
+
+            maxDeque.offerLast(numChecking)
+            minDeque.offerLast(numChecking)
+
+            if (nums[maxDeque.peekFirst()] - nums[minDeque.peekFirst()] <= limit) {
+                if (maxDeque.peekFirst() == result) {
+                    maxDeque.pollFirst()
                 }
-                result = maxOfResult(result, calculateResult(max, min, endIndex - startIndex + 1))
+                if (minDeque.peekFirst() == result) {
+                    minDeque.pollFirst()
+                }
+                result++
             }
+            numChecking++
         }
-//        var startIndex = 0
-//        while (startIndex < nums.size) {
-//            var endIndex = startIndex
-//            while (endIndex < nums.size) {
-//                var max = nums[startIndex]
-//                var min = nums[startIndex]
-//                for (i in startIndex..endIndex) {
-//                    max = maxOf(max, nums[i])
-//                    min = minOf(min, nums[i])
-//                }
-//                result = maxOfResult(result, calculateResult(max, min, endIndex - startIndex + 1))
-//                endIndex++
-//            }
-//            startIndex++
-//        }
         return result
     }
 }
