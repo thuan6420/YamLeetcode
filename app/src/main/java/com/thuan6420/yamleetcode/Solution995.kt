3974c17fb5e42b4007956edf68363885152b34e4
thuan6420 <thuan6420@gmail.com>
2024-06-26 16:33:58 +0700

    #995 - run exactly
    Runtime: 1076ms - beats: 25.00%
    Memory: 53.77Mb - beats: 28.57%

--- app/src/main/java/com/thuan6420/yamleetcode/Solution995.kt
+++ app/src/main/java/com/thuan6420/yamleetcode/Solution995.kt
@@ -3,6 +3,34 @@ package com.thuan6420.yamleetcode
 // 995. Minimum Number of K Consecutive Bit Flips in LeetCode
 class Solution995 {
     fun minKBitFlips(nums: IntArray, k: Int): Int {
-        return 0
+        val isNeedFlip: (Int) -> Boolean = { idx ->
+            nums[idx] == BIT_0
+        }
+        val isImpossibleFlip: (Int) -> Boolean = { idx ->
+            nums.size - idx < k
+        }
+        var res = 0
+        val flip: (Int) -> Unit = { startIdx ->
+            for (i in startIdx until startIdx + k) {
+                nums[i] = nums[i] xor BIT_1
+            }
+            res++
+        }
+        var idx = 0
+        while (idx < nums.size) {
+            if (isNeedFlip(idx)) {
+                if (isImpossibleFlip(idx)) {
+                    return -1
+                }
+                flip(idx)
+            }
+            idx++
+        }
+        return res
+    }
+
+    companion object {
+        const val BIT_1 = 1
+        const val BIT_0 = 0
     }
 }
